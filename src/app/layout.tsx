c2563ee0b1f9bc0a6b31a1544fde14103a4f690c
RHThree <russelheiser@gmail.com>
2024-11-01 18:10:29 -0400

    refactor: cleaned up home page code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,11 +44,11 @@ export default function RootLayout(props: any) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" /> {/* Add favicon link */}
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </head>
       <body className={poppins.variable}>
         <AppRouterCacheProvider>
